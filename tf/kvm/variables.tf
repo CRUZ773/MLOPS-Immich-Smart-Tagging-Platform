9d8d37939ccee1a866fbb873ddac474d68315229
Daniela Cruz <113479974+CRUZ773@users.noreply.github.com>
2026-04-05 14:44:39 -0400

    Add default nodes variable with IP addresses

--- tf/kvm/variables.tf
+++ tf/kvm/variables.tf
@@ -6,9 +6,19 @@ variable "suffix" {
 variable "key" {
   description = "Name of the SSH key pair registered on Chameleon"
   type        = string
+  default     = "id_rsa_chameleon"
 }
 
 variable "reservation" {
   description = "UUID of the Chameleon flavor reservation"
   type        = string
+}
+
+variable "nodes" {
+  type = map(string)
+  default = {
+    "node1" = "192.168.1.11"
+    "node2" = "192.168.1.12"
+    "node3" = "192.168.1.13"
+  }
 }
